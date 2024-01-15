5a8ab47f38cede79e8f8e73053e65a165c366052
SegoleneLazarus <seg.laz@ik.me>
2024-01-16 00:00:55 +0100

    triangle blanc finito

--- TP3_exo1_triangle_blanc/main.cpp
+++ TP3_exo1_triangle_blanc/main.cpp
@@ -1,4 +1,5 @@
 #include "p6/p6.h"
+#include "glimac/default_shader.hpp"
 
 int main()
 {
@@ -9,13 +10,49 @@ int main()
      * HERE SHOULD COME THE INITIALIZATION CODE
      *********************************/
 
+    GLuint vbos[6];
+    glGenBuffers(6, vbos);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbos[1]);
+        GLfloat vertices[] = {
+            -0.5f, -0.5f,
+            0.5f, -0.5f,
+            0.0f,  0.5f
+        };
+        glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    GLuint vaos[6];
+    glGenVertexArrays(6, vaos);
+
+    glBindVertexArray(vaos[2]);
+        static constexpr GLuint vertex_attr_position = 0;
+        glEnableVertexAttribArray(vertex_attr_position);
+        glBindBuffer(GL_ARRAY_BUFFER, vbos[1]);
+            glVertexAttribPointer(vertex_attr_position, 
+                2, GL_FLOAT, GL_FALSE, 
+                2*sizeof(GLfloat), 0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
     // Declare your infinite update loop.
     ctx.update = [&]() {
         /*********************************
          * HERE SHOULD COME THE RENDERING CODE
          *********************************/
+         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindVertexArray(vaos[2]);
+            glimac::bind_default_shader();
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
     };
 
     // Should be done last. It starts the infinite loop.
     ctx.start();
+
+    glDeleteBuffers(6,vbos);
+    glDeleteVertexArrays(6,vaos);
 }
